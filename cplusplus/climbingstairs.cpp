42dddaa1a35f4408d6c875e3cc0bb760d5f25e0d
Daryl Valairan <darylvalairan@outlook.com>
2025-12-19 02:25:52 -0500

    CPP: Added climbing stairs through Dynamic Programming

--- cplusplus/climbingstairs.cpp
+++ cplusplus/climbingstairs.cpp
@@ -15,10 +15,25 @@ int climbingStairsRecursive(int n)
     return climbingStairsRecursive(n - 1) + climbingStairsRecursive(n - 2);
 }
 
+int climbingStairsWithDP(int n)
+{
+    int prev1 = 1;
+    int prev2 = 2;
+
+    for (int i = 2; i < n; i++)
+    {
+        int current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;
+}
 
 int main()
 {
     int start = 7;
-    cout << "Number of ways to climb the stairs: " << climbingStairsRecursive(start) << endl;
+    cout << "Number of ways to climb the stairs through recursion: " << climbingStairsRecursive(start) << endl;
+    cout << "Number of ways to climb the stairs through DP: " << climbingStairsWithDP(start) << endl;
     return 0;
 }
