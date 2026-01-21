47acbae67644226751f51615c498fd4155b91284
Daryl Valairan <darylvalairan@outlook.com>
2026-01-20 22:52:58 -0500

    PY: Added comprehensions for palinfrome and anagram

--- python/buyandsellstock.py
+++ python/buyandsellstock.py
@@ -15,3 +15,4 @@ def buyandsell(prices):
 print(buyandsell([7,1,5,3,6,4]))
 print(buyandsell([7,6,4,3,1]))
 
+
---
+++ python/palindrome.py
@@ -0,0 +1,12 @@
+def isPalindrom(input):
+    filtered = ''.join(char.lower() for char in input if char.isalnum())
+    return filtered == filtered[::-1]
+
+is_palindrome = lambda s: (cleaned := "".join(character for character in s if character.isalnum()))== cleaned[::-1]
+is_anagram = lambda s1, s2: ({character: s1.count(character) for character in s1 if character.isalnum()}) == ( {character: s2.count(character) for character in s1 if character.isalnum()})
+
+print (is_palindrome("tomato"))
+print (is_palindrome("yo banana boy"))
+
+print (is_anagram("tomato", "potato"))
+print (is_anagram("listen", "silent"))
